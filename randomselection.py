f72cbbd3252cd12c6d7c8c901f08a78dbc4f0e23
hanquanphoon <51771552+hanquanphoon@users.noreply.github.com>
2019-07-23 19:02:11 +0800

    Update randomselection.py

--- randomselection.py
+++ randomselection.py
@@ -1,2 +1,34 @@
-def count(ballots,n):
-	
+def count(ballots,n,candidates):
+    quota = math.ceil(len(ballots)/(n+1))
+    tally = {}
+    elected = {}
+	transfer = {}  
+    
+    for i in candidates:
+        tally[i] = 0
+        
+    for i in ballots:
+        tally[i[0]] += 1
+    
+    for i in tally:
+        if tally[i] >= quota:
+            elected[i] = tally[i]
+
+	for i in candidates:
+		transfer[i] = []
+	for i in ballots:
+		if len(i) > 1:
+			transfer[i[0]].append(i)
+
+	return quota, tally, elected, transfer
+
+def checksurplus():
+	for i in elected:
+		if len(transfer[elected]) < elected[i]-quota:
+			electedexhausted = []
+			electedexhausted.append[i]
+	for i in ballots:
+		if i[0] in electedexhauted:
+			
+
+def distribute(
